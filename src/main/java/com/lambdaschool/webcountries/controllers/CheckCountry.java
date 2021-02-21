a73de76b4b7616c6b6eee3a7c70ba119aa49c8e7
Mikayla <mikaylabryant25@gmail.com>
2021-02-21 16:54:29 -0500

    completed first letter endpoint

---
+++ src/main/java/com/lambdaschool/webcountries/controllers/CheckCountry.java
@@ -0,0 +1,9 @@
+package com.lambdaschool.webcountries.controllers;
+
+import com.lambdaschool.webcountries.models.Country;
+
+public interface CheckCountry
+{
+    boolean test(Country c);
+
+}
--- src/main/java/com/lambdaschool/webcountries/controllers/CountryController.java
+++ src/main/java/com/lambdaschool/webcountries/controllers/CountryController.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 @RestController
 public class CountryController {
@@ -17,7 +19,20 @@ public class CountryController {
     @Autowired
     private CountryRepository countryrepos;
 
-    //http://localhost:2022/names/all
+    private List<Country> findCountries(List<Country> myList, CheckCountry tester)
+    {
+        List<Country> tempList = new ArrayList<>();
+        for (Country c : myList)
+        {
+            if(tester.test(c))
+            {
+                tempList.add(c);
+            }
+        }
+        return tempList;
+    }
+
+    //http://localhost:2022/names/all ✔
     @GetMapping(value="/names/all", produces = {"application/json"})
     public ResponseEntity<?> listAllCountries()
     {
@@ -28,8 +43,18 @@ public class CountryController {
 
     }
 
-    //http://localhost:2022/names/start/u
-
+    //http://localhost:2022/names/start/u ✔
+    @GetMapping(value="/names/start/{letter}", produces = {"application/json"})
+    public ResponseEntity<?> listAllByName(@PathVariable char letter)
+    {
+        List<Country> myList = new ArrayList<>();
+        countryrepos.findAll().iterator().forEachRemaining(myList::add); // creates array of all countries
+        List<Country> rtnList = findCountries(myList, c -> c.getName().toLowerCase().charAt(0) == letter);
+        // creating temporary list
+        // calling method that takes a list and a lambda expression
+        // lambda expression gets name of each country, then checks if the first letter is equal to the path variable
+        return new ResponseEntity<>(rtnList, HttpStatus.OK);
+    }
 
     //http://localhost:2022/population/total
 
