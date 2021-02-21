46e9c6655b91d976fcf95ec22f62671533e4a863
Mikayla <mikaylabryant25@gmail.com>
2021-02-21 04:45:49 -0500

    initialized project

---
+++ src/test/java/com/lambdaschool/webcountries/WebCountriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.webcountries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCountriesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
